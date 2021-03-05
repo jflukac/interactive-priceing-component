4c32d83388e898aea257cb6e0e1341601599ac50
jflukac <juanfranciscolukac@gmail.com>
2021-03-05 14:38:12 -0300

    Starting with JS

--- js/front.js
+++ js/front.js
@@ -1,3 +1,23 @@
 window.addEventListener('load', () => {
-    console.log('bien linkeado');
+    let qs = function(element) {
+        return document.querySelector(element)
+    }
+    let qsa = function(element) {
+        return document.querySelectorAll(element)
+    }
+    let geId = function(element) {
+        return document.getElementById(element)
+    }
+    let pageViews = qs('.pageViews')
+    let monthlyPrice = qs('.monthlyPrice')
+    let priceRange = geId('priceRange')
+
+
+    priceRange.addEventListener('change', () => {
+        console.log(priceRange.value);
+        pageViews.innerHTML = `${priceRange.value}K PAGEVIEWS`
+        monthlyPrice.innerHTML = `<span class="bold"> $${priceRange.value}</span>/month`
+    })
+    
+    
 })
